0a943ddcb60a083c81f4007de897b9dcddc6aec2
shohag.mia <shohag.mia@bracits.com>
2024-07-14 18:10:43 +0600

    rename for model to models

--- WebsiteOrUrlOnlineStatus/Program.cs
+++ WebsiteOrUrlOnlineStatus/Program.cs
@@ -9,16 +9,16 @@ namespace WebsiteOrUrlOnlineStatus
     {
         public static async Task Main(string[] args)
         {
-            var model = new List<UrlStatusModel>();
+            var models = new List<UrlStatusModel>();
 
             var urls = DataUrls.Urls();
             foreach (var url in urls)
             {
                 //var response = await EndPointIsService.IsSuccessPingStatus(url);
                 var response = await EndPointIsService.IsSuccessResponseStatus(url);
-                model.Add(new UrlStatusModel { Url = url, Status = response });
+                models.Add(new UrlStatusModel { Url = url, Status = response });
             }
-            foreach (var url in model)
+            foreach (var url in models)
             {
                 var message = $"The alive status code-{(int)url.Status} and Description- {CommonEnum.GetEnumDescription(url.Status)} from the URL- {url.Url}";
                 await Console.Out.WriteLineAsync(message);
